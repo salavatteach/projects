cef8cece23074f60925e610b2220dc7bcf48970e
salavatteach <salavat.seit.23981165@gmail.com>
2025-09-03 00:07:07 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-!function(){let s=document.getElementById("tasks"),e=document.getElementById("egor"),t=document.getElementById("tasksBlock"),l=document.getElementById("egorBlock"),i=document.getElementById("burger"),o=document.getElementById("sidebar"),c=document.getElementById("overlay"),d=document.getElementById("body");s.onclick=function(){e.classList.remove("active"),this.classList.add("active"),t.classList.remove("hidden"),l.classList.add("hidden"),o.classList.remove("show"),c.classList.remove("show")},e.onclick=function(){s.classList.remove("active"),this.classList.add("active"),l.classList.remove("hidden"),t.classList.add("hidden"),o.classList.remove("show"),c.classList.remove("show")},i.onclick=function(){d.classList.toggle("overflow"),c.classList.toggle("show"),o.classList.toggle("show")}}();
+!function() {
+  let s = document.getElementById("tasks"),
+      e = document.getElementById("egor"),
+      p = document.getElementById("monisensa"),         // ← новый таб
+      t = document.getElementById("tasksBlock"),
+      l = document.getElementById("egorBlock"),
+      pr = document.getElementById("monisensaBlock"),   // ← новый блок
+      i = document.getElementById("burger"),
+      o = document.getElementById("sidebar"),
+      c = document.getElementById("overlay"),
+      d = document.getElementById("body");
+
+  s.onclick = function() {
+    s.classList.add("active");
+    e.classList.remove("active");
+    p.classList.remove("active");
+
+    t.classList.remove("hidden");
+    l.classList.add("hidden");
+    pr.classList.add("hidden");
+
+    o.classList.remove("show");
+    c.classList.remove("show");
+  };
+
+  e.onclick = function() {
+    s.classList.remove("active");
+    e.classList.add("active");
+    p.classList.remove("active");
+
+    t.classList.add("hidden");
+    l.classList.remove("hidden");
+    pr.classList.add("hidden");
+
+    o.classList.remove("show");
+    c.classList.remove("show");
+  };
+
+  p.onclick = function() {
+    s.classList.remove("active");
+    e.classList.remove("active");
+    p.classList.add("active");
+
+    t.classList.add("hidden");
+    l.classList.add("hidden");
+    pr.classList.remove("hidden");
+
+    o.classList.remove("show");
+    c.classList.remove("show");
+  };
+
+  i.onclick = function() {
+    d.classList.toggle("overflow");
+    c.classList.toggle("show");
+    o.classList.toggle("show");
+  };
+}();
